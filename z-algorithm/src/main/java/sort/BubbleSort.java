45f21c5b8ac33e82f8979de8da8e21558bdf3694
zhaoxu1943 <463156880@qq.com>
2022-08-23 19:30:52 +0800

    sort-start

---
+++ z-algorithm/src/main/java/sort/BubbleSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+/**
+ * base:
+ * 冒泡排序
+ * @author zhaoxu
+ * @date 2022/8/23 11:18
+ * @since
+ */
+public class BubbleSort {
+    
+    
+    private int[] bubbleSortBase(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < ; j++) {
+                
+            }
+        }
+    }
+    
+    
+    private int[] bubbleSort1(int[] a) {
+        
+    }
+    
+    
+    
+}
