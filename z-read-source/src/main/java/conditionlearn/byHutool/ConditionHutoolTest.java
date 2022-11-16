51e0182c206e17cdf4df712115fd665e1b50e472
zhaoxu1943 <463156880@qq.com>
2022-11-16 11:07:43 +0800

    learn condition from hutool

---
+++ z-read-source/src/main/java/conditionlearn/byHutool/ConditionHutoolTest.java
@@ -0,0 +1,17 @@
+package conditionlearn.byHutool;
+
+import cn.hutool.db.sql.Condition;
+
+/**
+ * @author zhaoxu
+ * @date 11/15/2022 5:11 PM
+ * @since
+ */
+public class ConditionHutoolTest {
+
+
+    public static void main(String[] args) {
+        //hutool
+        Condition condition = new Condition();
+    }
+}
