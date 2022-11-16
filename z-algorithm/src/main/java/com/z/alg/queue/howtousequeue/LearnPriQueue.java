391605ac00fc469faf136129d90350f1e975470e
zhaoxu1943 <463156880@qq.com>
2022-11-16 11:16:08 +0800

    learn condition from hutool

---
+++ z-algorithm/src/main/java/com/z/alg/queue/howtousequeue/LearnPriQueue.java
@@ -0,0 +1,20 @@
+package com.z.alg.queue.howtousequeue;
+
+import java.util.PriorityQueue;
+
+/**
+ * homework: PriorityQueue Learning
+ * @author zhaoxu
+ * @date 11/16/2022 11:09 AM
+ * @since
+ */
+public class LearnPriQueue {
+
+    public static void main(String[] args) {
+
+        // based on priority heap
+        PriorityQueue<String> queue = new PriorityQueue<>();
+
+
+    }
+}
