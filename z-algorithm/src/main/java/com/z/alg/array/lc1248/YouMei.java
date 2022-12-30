b6aabac99683602f525259ce135a33811b4c7b10
zhaoxu1943 <463156880@qq.com>
2022-12-30 16:28:49 +0800

    a bad day,  i ll never go outside without c

--- z-algorithm/src/main/java/com/z/alg/array/lc1248/YouMei.java
+++ z-algorithm/src/main/java/com/z/alg/array/lc1248/YouMei.java
@@ -21,7 +21,7 @@ public class YouMei {
             //将计算改为查hash
             Map<Integer, Integer> sumMap = new HashMap<>();
             sumMap.put(0,0);
-            //fill sum array
+            //fill sum array,  from 1 to nums.length
             for (int i = 1; i <= nums.length; i++) {
                 int temp = 0;
                 for (int j = 0; j <= i-1; j++) {
@@ -29,8 +29,22 @@ public class YouMei {
                 }
                 sumMap.put(i,temp);
             }
+            [1,1,1,1,1];
+            sum [1,2,3,4,5]
+            1,1;
+            2,1;
+            3,1;
+            4,1;
+            5,1;
+
+
 
             //2. lianxu ziji,计算j-i
+            //继续转化: ,目前是双循环,通过i的递增,子数组窗口逐渐变大
+            //而第二层的遍历实际上可以转换
+            // sum[i] - sum[j-1] == k
+            //相当于 sum[j-1] = sum[i] - k
+
             for (int i = 1; i <= nums.length; i++) {
                 for (int j = 1; j <= i; j++) {
                     if(sumMap.get(i)-sumMap.get(j-1)==k){
@@ -60,6 +74,32 @@ public class YouMei {
         int res = obj.numberOfSubarrays(var1,var2);
         System.out.println(res);
 
+    }
+
+    /**
+     *
+     * lc写法:
+     * 首先明确再求出前缀和数组之后:双循环判定 sum[i]- sum[j-1] == k, res++; 将超时
+     * @author zhaoxu
+     */
+    class Solution {
+        public int numberOfSubarrays(int[] nums, int k) {
+            // 数组 prefixCnt 的下标是前缀和（即当前奇数的个数），值是前缀和的个数。
+            //init
+            int[] prefixCnt = new int[nums.length + 1];
+            prefixCnt[0] = 1;
+            // 遍历原数组，计算当前的前缀和，统计到 prefixCnt 数组中，
+            // 并且在 res 中累加上与当前前缀和差值为 k 的前缀和的个数。
+            int res = 0, sum = 0;
+            for (int num: nums) {
+                sum += num & 1;
+                prefixCnt[sum]++;
+                if (sum >= k) {
+                    res += prefixCnt[sum - k];
+                }
+            }
+            return res;
+        }
     }
 
 
