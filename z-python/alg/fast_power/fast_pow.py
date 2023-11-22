8eb353ad17b4aba400c75ba030034274e7ddf86e
zhaoxu1943 <463156880@qq.com>
2023-11-22 17:57:20 +0800

    feat(py): fast_pow算法

--- z-python/alg/fast_power/fast_pow.py
+++ z-python/alg/fast_power/fast_pow.py
@@ -1,10 +1,16 @@
 # 快速幂算法计算
 
-def slow_pow(num, power):
-    if (power & 1 == 1):
-        power>>2
+def binary_exp(num, power):
+    res = 1
+    while power != 0:
+        if (power & 1 == 1):
+            temp = num
+            res = res * temp
+            power = power >> 1
+    return res
+
 
 if __name__ == '__main__':
     a = 2
     n = 3
-    print(slow_pow(a, n))
+    print(binary_exp(a, n))
